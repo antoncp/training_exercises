43d62da2b977fb90eb53f6579ca978bc5cd90051
antoncp <antoncp@gmail.com>
2023-06-03 11:59:45 +0200

    Two more assignments

---
+++ yandex_contest/mother.py
@@ -0,0 +1,31 @@
+# ! change LOCAL to False before submitting !
+# set LOCAL to True for local testing
+
+LOCAL = False
+
+if LOCAL:
+    class Node:  
+        def __init__(self, value, next_item=None):  
+            self.value = value  
+            self.next_item = next_item
+
+def solution(node, elem):
+    idx = 0
+    while node.value != elem:
+        if not node.next_item:
+            return -1
+        node = node.next_item
+        idx += 1
+    return idx
+
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    idx = solution(node0, "node2")
+    assert idx == 2
+
+if __name__ == '__main__':
+    test()
