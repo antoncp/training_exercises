7f58d8821f0dd4824910bfadea8b1f69077cacf0
antoncp <antoncp@gmail.com>
2023-06-30 11:01:16 +0200

    New quicksort

--- yandex_contest/quicksort.py
+++ yandex_contest/quicksort.py
@@ -1,36 +1,49 @@
-# Задача "Эффективная быстрая сортировка". ID решения в Я.Контесте 88580246
+# Задача "Эффективная быстрая сортировка". ID решения в Я.Контесте 88616542
 
-from typing import List, Tuple
+from dataclasses import dataclass
+from typing import List
 
 
-def partition(array: Tuple[str, int, int], left: int, right: int) -> int:
+@dataclass
+class User:
+    """Represents users for comparison, their names, finished tasks and
+    accumulated fines.
+    """
+    name: str
+    tasks: int 
+    fine: int
+
+    @classmethod
+    def from_text(cls, text: str):
+        name, tasks, fine = text.split()
+        return cls(name=name, tasks=int(tasks), fine=int(fine))
+
+    def __str__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return (-self.tasks, self.fine, self.name) < (
+            -other.tasks,
+            other.fine,
+            other.name,
+        )
+
+
+def partition(array: List[User], left: int, right: int) -> int:
     """Compares elements of the array with pivot element, arranges elements
     based on their relation to the pivot element.
     """
     pivot = array[right]
     divider = left - 1
     for element in range(left, right):
-        if compare_users(array[element], pivot):
+        if array[element] < pivot:
             divider += 1
             array[divider], array[element] = array[element], array[divider]
     array[divider + 1], array[right] = array[right], array[divider + 1]
     return divider + 1
 
 
-def compare_users(
-    user_1: Tuple[str, int, int], user_2: Tuple[str, int, int]
-) -> bool:
-    """Compares two users by finished tasks, accumulated fines and names."""
-    return (-user_1[1], user_1[2], user_1[0]) < (
-        -user_2[1],
-        user_2[2],
-        user_2[0],
-    )
-
-
-def quicksort(
-    array: List[List[Tuple[str, int, int]]], left: int, right: int
-) -> List[List[Tuple[str, int, int]]]:
+def quicksort(array: List[User], left: int, right: int) -> List[User]:
     """Sorts array of users with quicksort algorithm."""
     if left < right:
         pivot = partition(array, left, right)
@@ -40,8 +53,7 @@ def quicksort(
 
 
 if __name__ == "__main__":
-    num = int(input())
-    users = [input().split() for _ in range(num)]
-    users = [(name, int(tasks), int(fine)) for name, tasks, fine in users]
-    for user in quicksort(users, left=0, right=num - 1):
-        print(user[0])
+    num: int = int(input())
+    users: List[User] = [User.from_text(input()) for _ in range(num)]
+    sorted_users: List[User] = quicksort(users, left=0, right=num-1)
+    print(*sorted_users, sep='\n')
