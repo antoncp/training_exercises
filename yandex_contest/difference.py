b3074376fd55dce1b59b62f05fae99399c1778ee
antoncp <antoncp@gmail.com>
2023-08-16 16:05:40 +0200

    Updates difference.py

--- yandex_contest/difference.py
+++ yandex_contest/difference.py
@@ -1,11 +1,15 @@
+import heapq
+
+
 def k_difference(n, areas, k):
     differences = []
     for i in range(n):
         for j in range(i + 1, n):
             diff = abs(areas[i] - areas[j])
-            differences.append(diff)
-    differences.sort()
-    return differences[k - 1]
+            heapq.heappush(differences, diff)
+            if len(differences) > k:
+                heapq.heappop(differences)
+    return heapq.heappop(differences)
 
 
 n = int(input())
