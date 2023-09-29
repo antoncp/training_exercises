8141f684a61fe64d79af0c0968401f796f82ada1
antoncp <antoncp@gmail.com>
2023-09-29 10:38:09 +0200

    Sort Array By Parity

---
+++ SortArray.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        even = [i for i in nums if i % 2 == 0]
+        odd = [i for i in nums if i % 2 != 0]
+        return even + sorted(odd)
