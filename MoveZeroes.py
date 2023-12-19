5eecc9f9bb2140bc61cffdd97a9d55150e285369
antoncp <antoncp@gmail.com>
2023-12-19 21:43:10 +0500

    Move Zeroes

---
+++ MoveZeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(i)
+        return nums
