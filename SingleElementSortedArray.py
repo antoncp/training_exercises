1cfa17cb208ecbc08921d9037356bdf920435989
antoncp <antoncp@gmail.com>
2024-01-03 12:48:06 +0100

    Single Element in a Sorted Array

---
+++ SingleElementSortedArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNonDuplicate(self, nums):
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
+            else:
+                right = mid
+        return nums[left]
